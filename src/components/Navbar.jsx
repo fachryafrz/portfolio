59b36378254783d59049fee43c5b0e3498eeebeb
fachryafrz <fachry.dwi.fd.fda@gmail.com>
2023-05-19 09:11:16 +0700

    Update: Navbar btn outline 0

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,19 +3,19 @@ export default function Navbar() {
     <nav className="flex items-center py-6 justify-center gap-4 sm:gap-12 bg-bg bg-opacity-90 backdrop-blur-sm">
       <a
         href="#about"
-        className="flex items-center gap-1 before:content-['01.'] before:text-accent hover:text-white transition-all focus:outline-none focus:text-white"
+        className="flex items-center gap-1 before:content-['01.'] before:text-accent hover:text-white transition-all focus:outline-0 focus:text-white"
       >
         About
       </a>
       <a
         href="#projects"
-        className="flex items-center gap-1 before:content-['02.'] before:text-accent hover:text-white transition-all focus:outline-none focus:text-white"
+        className="flex items-center gap-1 before:content-['02.'] before:text-accent hover:text-white transition-all focus:outline-0 focus:text-white"
       >
         Projects
       </a>
       <a
         href="#contact"
-        className="flex items-center gap-1 before:content-['03.'] before:text-accent hover:text-white transition-all focus:outline-none focus:text-white"
+        className="flex items-center gap-1 before:content-['03.'] before:text-accent hover:text-white transition-all focus:outline-0 focus:text-white"
       >
         Contact
       </a>
