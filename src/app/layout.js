56fe1ba3c067340f07e858f1f2e2c629e4556b8f
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2025-03-11 22:31:07 +0700

    feat: add Open Graph and robots metadata, update .env.example, and modify .gitignore

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,39 @@ import SmoothScroll from "@/components/smooth-scroll";
 export const metadata = {
   title: process.env.NEXT_PUBLIC_APP_NAME,
   description: process.env.NEXT_PUBLIC_APP_DESCRIPTION,
+  openGraph: {
+    siteName: process.env.NEXT_PUBLIC_APP_NAME,
+    title: process.env.NEXT_PUBLIC_APP_NAME,
+    description: process.env.NEXT_PUBLIC_APP_DESCRIPTION,
+    url: process.env.NEXT_PUBLIC_APP_URL,
+    images: {
+      url: "/maskable/maskable_icon_x512.png",
+      width: 512,
+      height: 512,
+    },
+  },
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/favicon/light/favicon.ico",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/favicon/dark/favicon.ico",
+      },
+    ],
+  },
+  robots: {
+    index: true,
+    follow: false,
+    noimageindex: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+    },
+  },
 };
 
 export default function RootLayout({ children }) {
