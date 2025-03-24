5ebc4243ca5e6aa1c84fc0bca8e3e9f963ec44bc
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2025-03-24 14:21:43 +0700

    refactor: remove unused schema types and update structure for portfolio

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,12 +1,9 @@
 import { type SchemaTypeDefinition } from "sanity";
 
 import { blockContentType } from "./blockContentType";
-import { categoryType } from "./categoryType";
-import { postType } from "./postType";
-import { authorType } from "./authorType";
 import { projectType } from "./projectType";
 import { technologyType } from "./technologyType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, categoryType, postType, authorType, projectType, technologyType],
+  types: [blockContentType, projectType, technologyType],
 };
