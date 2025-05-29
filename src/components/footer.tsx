ad82021a8eef7af67e335cd66137e4de0443b2e2
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2025-05-29 22:13:01 +0700

    refactor: remove Twitter and Instagram links, retain LinkedIn and GitHub

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Github, Instagram, Linkedin, Mail, Twitter } from "lucide-react";
+import { Github, Linkedin, Mail } from "lucide-react";
 import Link from "next/link";
 
 export default function Footer() {
@@ -12,28 +12,16 @@ export default function Footer() {
       ariaLabel: "Check out my GitHub profile",
     },
     {
-      name: "LinkedIn",
-      icon: <Linkedin />,
-      url: "https://linkedin.com/in/fachryafrz",
-      ariaLabel: "Check out my LinkedIn profile",
-    },
-    {
       name: "GMail",
       icon: <Mail />,
       url: "mailto:fachrydwiafriza@gmail.com",
       ariaLabel: "Send me an email",
     },
     {
-      name: "Twitter",
-      icon: <Twitter />,
-      url: "https://twitter.com/fachryafrz",
-      ariaLabel: "Check out my Twitter profile",
-    },
-    {
-      name: "Instagram",
-      icon: <Instagram />,
-      url: "https://instagram.com/fachryafrz",
-      ariaLabel: "Check out my Instagram profile",
+      name: "LinkedIn",
+      icon: <Linkedin />,
+      url: "https://linkedin.com/in/fachryafrz",
+      ariaLabel: "Check out my LinkedIn profile",
     },
   ];
 
